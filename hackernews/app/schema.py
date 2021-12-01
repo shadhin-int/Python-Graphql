e77914cb12e2b344c663a533571de0dee368eb35
shadhin-int <shadhin.int@gmail.com>
2021-12-01 13:56:38 +0600

    added tokenauth

--- hackernews/app/schema.py
+++ hackernews/app/schema.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 import graphene
+from graphql_jwt import refresh_token
 import links.schema
 import user.schema
+import graphql_jwt
 
 class Query(links.schema.Query, user.schema.Query, graphene.ObjectType):
     pass
@@ -9,6 +11,8 @@ class Query(links.schema.Query, user.schema.Query, graphene.ObjectType):
 # schema = graphene.Schema(query=Query)
 
 class Mutation(links.schema.Mutation, user.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    veryfy_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=Query,mutation=Mutation)
