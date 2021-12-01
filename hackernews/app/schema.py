3ab1bf5e99466d067960f6f9eb30b6e5190cf8e2
shadhin-int <shadhin.int@gmail.com>
2021-12-01 13:48:12 +0600

    added schema for create and get user

--- hackernews/app/schema.py
+++ hackernews/app/schema.py
@@ -1,14 +1,14 @@
+from django.contrib.auth.models import User
 import graphene
 import links.schema
+import user.schema
 
-class Query(links.schema.Query, graphene.ObjectType):
+class Query(links.schema.Query, user.schema.Query, graphene.ObjectType):
     pass
 
 # schema = graphene.Schema(query=Query)
 
-class Mutation(links.schema.Mutation, graphene.ObjectType):
+class Mutation(links.schema.Mutation, user.schema.Mutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query,mutation=Mutation)
-
-
