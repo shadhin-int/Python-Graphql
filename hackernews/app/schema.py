17e8b0aafde3964a31d1ec272b078a0a0f37360f
shadhin-int <shadhin.int@gmail.com>
2021-12-02 18:04:14 +0600

    added reply structure

--- hackernews/app/schema.py
+++ hackernews/app/schema.py
@@ -1,18 +1,29 @@
 from django.contrib.auth.models import User
 import graphene
 from graphql_jwt import refresh_token
+from links.models import Link
 import links.schema
 import user.schema
 import graphql_jwt
+# import links.schema_relay
+from links import schema_relay
 
-class Query(links.schema.Query, user.schema.Query, graphene.ObjectType):
+
+class Query(
+    links.schema.Query,
+    user.schema.Query,
+    links.schema_relay.RelayQuery,
+    graphene.ObjectType
+):
     pass
 
 # schema = graphene.Schema(query=Query)
+
 
 class Mutation(links.schema.Mutation, user.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     veryfy_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
 
-schema = graphene.Schema(query=Query,mutation=Mutation)
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
