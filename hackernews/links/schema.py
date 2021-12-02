b1f774c2e55cffae1a60dfa346afa0085c74299e
shadhin-int <shadhin.int@gmail.com>
2021-12-02 17:13:02 +0600

    added pagination

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -12,27 +12,40 @@ class LinkType(DjangoObjectType):
     class Meta:
         model = Link
 
+
 class VoteType(DjangoObjectType):
     class Meta:
         model = Vote
 
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType, search=graphene.String())
+    links = graphene.List(
+        LinkType,
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int()
+    )
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info, search=None, **kwargs):
+    def resolve_links(self, info, search=None, first=None, skip=None, **kwargs):
+        qs = Link.objects.all()
+
         if search:
             filter = (
                 Q(url__icontains=search) | Q(description__icontains=search)
             )
-            return Link.objects.filter(filter)
-        return Link.objects.all()
+            qs = qs.filter(filter)
 
+        if skip:
+            qs = qs[skip:]
 
-    def resolve_votes(self,info, **kwargs):
+        if first:
+            qs = qs[:first]
+
+        return qs
+
+    def resolve_votes(self, info, **kwargs):
         return Vote.objects.all()
-
 
 
 class CreateLink(graphene.Mutation):
